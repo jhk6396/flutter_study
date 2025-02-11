1c933c90571f8a97206eb12ef7e8a606573eb5c9
권재환 <kyg9539@naver.com>
2025-02-11 17:53:07 +0900

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/common/component/custom_text_form_field.dart';
+import 'package:flutter_study/user/view/login_scren.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
